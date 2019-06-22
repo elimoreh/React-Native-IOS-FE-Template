6284c854b17496ca56f81b7a8184d1024163a724
elimoreh <elimoreh@berkeley.edu>
2019-06-21 20:04:46 -0700

    change tab names

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -10,61 +10,13 @@ import {
   View,
 } from 'react-native';
 
-import { MonoText } from '../components/StyledText';
+// import { Text } from '../components/StyledText';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
-
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
+      <ScrollView>
       </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
-      </View>
     </View>
   );
 }
@@ -111,7 +63,7 @@ function handleHelpPress() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#5dce9a',
   },
   developmentModeText: {
     marginBottom: 20,
@@ -196,3 +148,41 @@ const styles = StyleSheet.create({
     color: '#2e78b7',
   },
 });
+
+
+
+       // style={styles.container}
+        // contentContainerStyle={styles.contentContainer}>
+        // <View style={styles.welcomeContainer}>
+        //   <Image
+        //     source={
+        //       __DEV__
+        //         ? require('../assets/images/robot-dev.png')
+        //         : require('../assets/images/robot-prod.png')
+        //     }
+        //     style={styles.welcomeImage}
+        //   />
+        // </View>
+
+        // <View style={styles.getStartedContainer}>
+        //   <DevelopmentModeNotice />
+
+        //   <Text style={styles.getStartedText}>Get started by opening</Text>
+
+        //   <View
+        //     style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+        //     <Text>screens/HomeScreen.js</Text>
+        //   </View>
+
+        //   <Text style={styles.getStartedText}>
+        //     Change this text and your app will automatically reload.
+        //   </Text>
+        // </View>
+
+        // <View style={styles.helpContainer}>
+        //   <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
+        //     <Text style={styles.helpLinkText}>
+        //       Help, it didn’t automatically reload!
+        //     </Text>
+        //   </TouchableOpacity>
+        // </View>
